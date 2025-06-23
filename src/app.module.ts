91023f6eda8b39193bc8e232599182037398890e
Nguyen Sam <sam.np@geekup.vn>
2025-06-24 00:13:32 +0700

    update source be, config entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 // src/app.module.ts
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import { UsersModule } from './modules/users/users.module';
 import { ProductsModule } from './modules/products/products.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { User } from './modules/users/entities/user.entity'; // Đảm bảo import đúng
 
 @Module({
-  imports: [AuthModule, UsersModule, ProductsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres', // hoặc 'mysql'
+      host: process.env.DB_HOST,
+      port: +(process.env.DB_PORT ?? 5432),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [User],
+      synchronize: true,
+    }),
+    AuthModule,
+    UsersModule,
+    ProductsModule,
+  ],
 })
 export class AppModule {}
